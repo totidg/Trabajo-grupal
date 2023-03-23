c8892556238965a623cc199a2b228dbc976de07e
totidg <113801644+totidg@users.noreply.github.com>
2023-03-23 00:25:25 -0300

    1 file modified

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const form = document.querySelector('form');
 const tabla = document.getElementById('tabla');
 
 form.addEventListener('submit', function (event) {
+  
   // previene submitear el formulario
   event.preventDefault();
 
-  // https://developer.mozilla.org/en-US/docs/Web/API/FormData
-  const formData = new FormData(event.target);
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries
+  // f.d Los objetos FormData permiten compilar un conjunto de pares clave/valor
+  // e.t referencia al objeto en el cual se lanzo el evento
+  const formData = new FormData(event.target); 
+  // El método Object.fromEntries() transforma una lista de pares con [clave-valor] en un objeto.
   const values = Object.fromEntries(formData);
 
   // Crea una fila dentro de tbody
@@ -35,5 +37,6 @@ form.addEventListener('submit', function (event) {
   tr1.appendChild(tdCorreo);
   tdCorreo.innerHTML = values.correo;
 
+  // Vaciar inputs del form
   form.reset()
 });
